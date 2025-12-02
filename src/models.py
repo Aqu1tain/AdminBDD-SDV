2f23889d46056c2f6ce70384a271e36bb8de92c9
Corentin Renard <corentinfox08@gmail.com>
2025-12-02 15:31:00 +0100

    refactor: extract Entity base class for Character and Monster

--- src/models.py
+++ src/models.py
@@ -1,39 +1,34 @@
-
-class Character:
-    def __init__(self, name, attack, defense, max_hp, current_hp):
+class Entity:
+    def __init__(self, name, attack, defense, hp):
         self.name = name
         self.attack = attack
         self.defense = defense
-        self.max_hp = max_hp
-        self.current_hp = current_hp
+        self.max_hp = hp
+        self.current_hp = hp
 
     def is_alive(self):
         return self.current_hp > 0
 
     def take_damage(self, damage):
-        self.current_hp -= damage
+        actual_damage = max(0, damage - self.defense)
+        self.current_hp = max(0, self.current_hp - actual_damage)
 
     def attack_target(self, target):
         target.take_damage(self.attack)
 
     def get_stats(self):
-        return self.current_hp, self.max_hp, self.attack, self.defense
+        return {
+            "name": self.name,
+            "current_hp": self.current_hp,
+            "max_hp": self.max_hp,
+            "attack": self.attack,
+            "defense": self.defense
+        }
 
-class Monster:
-    def __init__(self, name, attack, defense, hp):
-        self.name = name
-        self.defense = defense
-        self.attack = attack
-        self.hp = hp
 
-    def is_alive(self):
-        return self.hp > 0
+class Character(Entity):
+    pass
 
-    def take_damage(self, damage):
-        self.hp -= damage
 
-    def attack_player(self, player):
-        player.take_damage(self.attack)
-
-    def get_stats(self):
-        return self.hp, self.defense
+class Monster(Entity):
+    pass
