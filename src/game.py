33ea8fff7750a76370f638d4117b6a5e5abeba21
Corentin Renard <corentinfox08@gmail.com>
2025-12-04 17:16:05 +0100

    refactor: split functions to enforce single responsibility and line limits
    Breaking down multi-purpose functions into smaller, atomic operations to comply with project code principles (max 20 lines per function, single responsibility).
    
    Changes:
    - Extracted presentation logic from business logic across both files
    - Split orchestration functions into focused, testable units
    - All functions now under 20 lines and doing one thing well

--- src/game.py
+++ src/game.py
@@ -7,19 +7,30 @@ import random
 def print_monster_status(monster):
     print(MSG_MONSTER_STATUS.format(name=monster.name, current_hp=monster.current_hp, max_hp=monster.max_hp))
 
+def print_character_attack(character, monster, is_crit):
+    crit_text = MSG_CRITICAL_HIT if is_crit else ""
+    print(MSG_CHARACTER_ATTACK.format(name=character.name, current_hp=character.current_hp, max_hp=character.max_hp, target=monster.name, crit=crit_text))
+
 def team_attacks_monster(team, monster):
     for c in team:
         if c.is_alive():
             is_crit = c.attack_target(monster)
-            crit_text = MSG_CRITICAL_HIT if is_crit else ""
-            print(MSG_CHARACTER_ATTACK.format(name=c.name, current_hp=c.current_hp, max_hp=c.max_hp, target=monster.name, crit=crit_text))
+            print_character_attack(c, monster, is_crit)
+
+def get_cooldown_text(character):
+    if character.is_ability_ready():
+        return MSG_ABILITY_READY.format(ability=character.ability_name)
+    return MSG_ABILITY_COOLDOWN.format(ability=character.ability_name, cooldown=character.current_cooldown)
+
+def print_team_member_status(character):
+    cooldown_text = get_cooldown_text(character)
+    print(MSG_TEAM_MEMBER_STATUS.format(name=character.name, current_hp=character.current_hp, max_hp=character.max_hp, cooldown=cooldown_text))
 
 def print_team_status(team):
     print(MSG_TEAM_STATUS)
     for c in team:
         if c.is_alive():
-            cooldown_text = MSG_ABILITY_READY.format(ability=c.ability_name) if c.is_ability_ready() else MSG_ABILITY_COOLDOWN.format(ability=c.ability_name, cooldown=c.current_cooldown)
-            print(MSG_TEAM_MEMBER_STATUS.format(name=c.name, current_hp=c.current_hp, max_hp=c.max_hp, cooldown=cooldown_text))
+            print_team_member_status(c)
 
 def ask_ability_usage(character):
     if not character.is_ability_ready():
@@ -41,42 +52,49 @@ def handle_abilities(team, monster):
                 return True
     return False
 
-def end_turn_cleanup(team):
-    reduce_team_cooldowns(team)
-    print_team_status(team)
-
 def check_monster_defeated(monster):
     if not monster.is_alive():
         print(MSG_MONSTER_DEFEATED)
         return True
     return False
 
-def combat_turn(team : list[Character], monster : Monster):
-    print(MSG_COMBAT_TURN)
-    print_monster_status(monster)
-
-    if handle_abilities(team, monster) and check_monster_defeated(monster):
-        end_turn_cleanup(team)
-        return True
+def execute_player_turn(team, monster):
+    if handle_abilities(team, monster):
+        return check_monster_defeated(monster)
 
     team_attacks_monster(team, monster)
+    return check_monster_defeated(monster)
 
-    if check_monster_defeated(monster):
-        end_turn_cleanup(team)
-        return True
-
+def execute_monster_turn(monster, team):
     selected_c = monster_fight_character(monster, team)
     if not selected_c.is_alive():
         print(MSG_CHARACTER_DEFEATED.format(name=selected_c.name))
 
-    end_turn_cleanup(team)
+def combat_turn(team : list[Character], monster : Monster):
+    print(MSG_COMBAT_TURN)
+    print_monster_status(monster)
+
+    if execute_player_turn(team, monster):
+        reduce_team_cooldowns(team)
+        print_team_status(team)
+        return True
+
+    execute_monster_turn(monster, team)
+    reduce_team_cooldowns(team)
+    print_team_status(team)
     return False
 
-def monster_fight_character(monster : Monster, team : list[Character]):
-    c = random.choice(team)
-    is_crit = monster.attack_target(c)
+def select_random_target(team):
+    return random.choice(team)
+
+def print_monster_attack(monster, character, is_crit):
     crit_text = MSG_CRITICAL_HIT if is_crit else ""
-    print(MSG_MONSTER_ATTACK.format(name=monster.name, target=c.name, current_hp=c.current_hp, max_hp=c.max_hp, crit=crit_text))
+    print(MSG_MONSTER_ATTACK.format(name=monster.name, target=character.name, current_hp=character.current_hp, max_hp=character.max_hp, crit=crit_text))
+
+def monster_fight_character(monster : Monster, team : list[Character]):
+    c = select_random_target(team)
+    is_crit = monster.attack_target(c)
+    print_monster_attack(monster, c, is_crit)
     return c
 
 def is_team_alive(team):
@@ -98,24 +116,31 @@ def scale_monster_for_wave(monster, wave):
     monster.attack = int(monster.attack * stat_multiplier)
     monster.defense = int(monster.defense * stat_multiplier)
 
-def buff_team_after_wave(team, wave):
-    print(MSG_WAVE_COMPLETE.format(wave=wave))
+def apply_buffs_to_team(team):
     for c in team:
         if c.is_alive():
             c.attack += TEAM_ATTACK_BUFF
             c.defense += TEAM_DEFENSE_BUFF
             c.max_hp += TEAM_HP_BUFF
             c.current_hp = min(c.max_hp, c.current_hp + TEAM_HP_BUFF)
+
+def buff_team_after_wave(team, wave):
+    print(MSG_WAVE_COMPLETE.format(wave=wave))
+    apply_buffs_to_team(team)
     print(MSG_TEAM_BUFF)
+
+def prepare_wave(db, wave):
+    monster = get_random_monster(db)
+    scale_monster_for_wave(monster, wave)
+    print(MSG_WAVE_START.format(wave=wave, monster=monster.name))
+    return monster
 
 def start_combat(team : list[Character], db):
     wave = 0
 
     while is_team_alive(team):
         wave += 1
-        monster = get_random_monster(db)
-        scale_monster_for_wave(monster, wave)
-        print(MSG_WAVE_START.format(wave=wave, monster=monster.name))
+        monster = prepare_wave(db, wave)
 
         if not fight_monster(team, monster):
             break
--- src/main.py
+++ src/main.py
@@ -16,76 +16,98 @@ def get_menu_choice():
     except ValueError:
         return -1
 
+def print_character_info(index, character):
+    stats = character.get_stats()
+    print(MSG_CHAR_STATS.format(num=index, name=stats['name'], attack=stats['attack'], defense=stats['defense'], hp=stats['max_hp']))
+
 def display_characters(characters):
     print(MSG_CHAR_AVAILABLE)
-
     for i, char in enumerate(characters, 1):
-        stats = char.get_stats()
-        print(MSG_CHAR_STATS.format(num=i, name=stats['name'], attack=stats['attack'], defense=stats['defense'], hp=stats['max_hp']))
+        print_character_info(i, char)
+
+def get_character_choice_input(position):
+    try:
+        choice = int(input(MSG_TEAM_CHOICE_PROMPT.format(num=position)))
+        return choice
+    except ValueError:
+        return None
+
+def try_add_character_to_team(characters, team, choice):
+    if choice is None or choice < 1 or choice > len(characters):
+        print(MSG_MENU_INVALID_CHOICE)
+        return False
+
+    selected = characters.pop(choice - 1)
+    team.append(selected)
+    return True
 
 def select_team(db):
     characters = get_all_characters(db)
-
     team = []
+
     while len(team) < TEAM_SIZE:
         display_characters(characters)
         print(MSG_TEAM_CURRENT.format(team=[c.name for c in team]))
 
-        try:
-            choice = int(input(MSG_TEAM_CHOICE_PROMPT.format(num=len(team) + 1)))
-
-            if 1 <= choice <= len(characters):
-                selected = characters.pop(choice - 1)
-                team.append(selected)
-            else:
-                raise ValueError
-
-        except (ValueError, IndexError):
-            print(MSG_MENU_INVALID_CHOICE)
+        choice = get_character_choice_input(len(team) + 1)
+        try_add_character_to_team(characters, team, choice)
 
     return team
 
-def show_leaderboard(db):
-    print(MSG_LEADERBOARD_TITLE)
-    scores = get_top_scores(db)
-
+def print_leaderboard_entries(scores):
     if len(scores) == 0:
         print(MSG_LEADERBOARD_EMPTY)
     else:
         for i, entry in enumerate(scores, 1):
             print(MSG_LEADERBOARD_ENTRY.format(rank=i, username=entry['username'], score=entry['score']))
 
-def start_game(db):
-    username = input(MSG_USERNAME_PROMPT)
-    team = select_team(db)
+def show_leaderboard(db):
+    print(MSG_LEADERBOARD_TITLE)
+    scores = get_top_scores(db)
+    print_leaderboard_entries(scores)
 
+def print_selected_team(username, team):
     print(MSG_TEAM_FINAL.format(username=username))
     for char in team:
         print(MSG_TEAM_MEMBER.format(name=char.name))
 
+def start_game(db):
+    username = input(MSG_USERNAME_PROMPT)
+    team = select_team(db)
+
+    print_selected_team(username, team)
+
     score = start_combat(team, db)
     save_score(db, username, score)
+
+def handle_menu_choice(db, choice):
+    if choice == 1:
+        start_game(db)
+    elif choice == 2:
+        show_leaderboard(db)
+    elif choice == 3:
+        print(MSG_MENU_GOODBYE)
+        return False
+    else:
+        print(MSG_MENU_INVALID_CHOICE)
+    return True
+
+def run_game_loop(db):
+    while True:
+        show_main_menu()
+        choice = get_menu_choice()
+
+        if not handle_menu_choice(db, choice):
+            break
 
 def main():
     db = init_database()
 
-    if db is not False :
-        while True:
-            show_main_menu()
-            choice = get_menu_choice()
-
-            if choice == 1:
-                start_game(db)
-            elif choice == 2:
-                show_leaderboard(db)
-            elif choice == 3:
-                print(MSG_MENU_GOODBYE)
-                break
-            else:
-                print(MSG_MENU_INVALID_CHOICE)
-
-    else:
+    if db is False:
         print(MSG_DB_NO_CONNECTION)
+        return
+
+    run_game_loop(db)
 
 if __name__ == "__main__":
     main()
